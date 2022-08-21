99ba2916501e002dec0f8e12fa91643127f93d61
Victoria igbobi <60755803+victoria-igbobi65@users.noreply.github.com>
2022-08-21 06:32:37 +0000

    Added comments

--- index.js
+++ index.js
@@ -2,20 +2,23 @@
 const fs = require('fs')
 const path = require('path')
 
-
+// obtain filepath of the text file
 const namesFilePath = path.join(__dirname, 'names.txt')
 let name = prompt('Enter your name... ')
 
- 
+
+//Checks if file exists, if it does append user name to gthe file
 if (namesFilePath) {
   fs.appendFile(namesFilePath, `${name}\n`, (err) => {
     if (err) {
       return
     }
+    
     console.log('Name added successfully!')
   })
 }
 
+  // Else create the file and write the name to the file
 else {
   fs.writeFile(namesFilePath, `${name}`, (err) => {
     if (err) {
